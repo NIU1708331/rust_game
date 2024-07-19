a77dec8e56d61f026a7ffbb8e7c85a6d45552441
Daniel QIu <daniel.haocq@outlook.com>
2024-07-19 23:41:10 +0200

    diagonal acabat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use rand::Rng;
-use std::io::{self, Write};
+use std::{io::{self, Write}, process::id};
 
 const SIZE: usize = 15;
 
@@ -15,7 +15,7 @@ fn imprimir(matriu: [[i32; SIZE]; SIZE]) {
         for j in 0..SIZE {
             match matriu[i][j] {
                 0 => print!("_  "),
-                1 => print!("o  "),
+                1 => print!("O  "),
                 2 => print!("x  "),
                 _ => (),
             }
@@ -49,6 +49,89 @@ fn victoria(matriu: [[i32; SIZE]; SIZE], x: usize, y: usize, player: i32) -> boo
             conseq = 0;
         }
     }
+
+
+    //diagonal xy ++
+
+    for i in 1..SIZE {
+        if i+x>=SIZE || i+y>=SIZE {
+            break;
+        }
+        else {
+            if matriu[x+i][y+i]==player {
+                conseq +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    //diagonal xy --
+
+    for i in 1..SIZE {
+        if false{
+            break;
+        }
+        else {
+            if x<=i||y<=i {
+                break;
+            }
+            if matriu[x-i][y-i]==player {
+                conseq +=1;
+            }
+            else {
+                 break;
+            }
+        }
+    }
+
+    if conseq==5 {
+        return  true;
+    }
+
+    conseq=0;
+
+    //diagonal yx ++
+
+    for i in 1..SIZE {
+        if i+y>=SIZE{
+            break;
+        }
+        else {
+            if x<=i {
+                break;
+            }
+            if matriu[x-i][y+i]==player {
+                conseq +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    for i in 1..SIZE {
+        if i+x>=SIZE{
+            break;
+        }
+        else {
+            if y<=i {
+                break;
+            }
+            if matriu[x+i][y-i]==player {
+                conseq +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+     
+    if conseq==5 {
+        return  true;
+    }
+
 
     false
 }
@@ -86,28 +169,206 @@ fn count_consecutive(matrix: [[i32; SIZE]; SIZE], x: usize, y: usize, num: i32) 
         }
     }
 
-    count_x + count_y
+    //diagonal xy ++
+
+    let mut count_xy=0;
+
+    for i in 1..SIZE {
+        if i+x>=SIZE || i+y>=SIZE {
+            break;
+        }
+        else {
+            if matrix[x+i][y+i]==num {
+                count_xy +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    
+    //diagonal xy --
+
+    
+    
+    for i in 1..SIZE {
+        if false{
+            break;
+        }
+        else {
+            if x<=i||y<=i {
+                break;
+            }
+            if matrix[x-i][y-i]==num {
+                count_xy +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    //diagonal yx ++
+    let mut count_yx=0;
+    
+    for i in 1..SIZE {
+        if i+y>=SIZE{
+            break;
+        }
+        else {
+            if x<=i {
+                break;
+            }
+            if matrix[x-i][y+i]==num {
+                count_yx +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    
+    for i in 1..SIZE {
+        if i+x>=SIZE {
+           break;
+        }
+        else {
+            if y<=i {
+                break;
+            }
+            if matrix[x+i][y-i]==num {
+               count_yx +=1;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    let mut puntuacion=0;
+
+    if num==2 {
+        match count_x {
+            1 => puntuacion += 5,
+            2 => puntuacion += 50,
+            3 => puntuacion += 500,
+            4 => puntuacion += 99999,
+            _ => puntuacion += 0,
+        }
+        match count_y {
+            1 => puntuacion += 5,
+            2 => puntuacion += 50,
+            3 => puntuacion += 500,
+            4 => puntuacion += 99999,
+            _ => puntuacion += 0,
+        }
+        match count_xy {
+            1 => puntuacion += 5,
+            2 => puntuacion += 50,
+            3 => puntuacion += 500,
+            4 => puntuacion += 99999,
+            _ => puntuacion += 0,
+        }
+        match count_yx {
+            1 => puntuacion += 5,
+            2 => puntuacion += 50,
+            3 => puntuacion += 500,
+            4 => puntuacion += 99999,
+            _ => puntuacion += 0,
+        }
+    }
+    else {
+        if num==1 {
+            match count_x {
+                1 => puntuacion += 3,
+                2 => puntuacion += 50,
+                3 => puntuacion += 1000,
+                _ => puntuacion += 0,
+            }
+            match count_y {
+                1 => puntuacion += 3,
+                2 => puntuacion += 50,
+                3 => puntuacion += 1000,
+                _ => puntuacion += 0,
+            }
+            match count_xy {
+                1 => puntuacion += 3,
+                2 => puntuacion += 50,
+                3 => puntuacion += 1000,
+                _ => puntuacion += 0,
+            }
+            match count_yx {
+                1 => puntuacion += 3,
+                2 => puntuacion += 50,
+                3 => puntuacion += 1000,
+                _ => puntuacion += 0,
+            }
+        }
+        else {
+            match count_x {
+                1 => puntuacion += -100,
+                2 => puntuacion += -10,
+                3 => puntuacion += -1,
+                _ => puntuacion += 0,
+            }
+            match count_y {
+                1 => puntuacion += -100,
+                2 => puntuacion += -10,
+                3 => puntuacion += -1,
+                _ => puntuacion += 0,
+            }
+            match count_xy {
+                1 => puntuacion += -100,
+                2 => puntuacion += -10,
+                3 => puntuacion += -1,
+                _ => puntuacion += 0,
+            }
+            match count_yx {
+                1 => puntuacion += -100,
+                2 => puntuacion += -10,
+                3 => puntuacion += -1,
+                _ => puntuacion += 0,
+            }
+        }
+    }
+
+/*     println!("{} ,{} ,{} ,{}",count_x,count_y,count_xy,count_yx);
+    println!("{}",puntuacion); */
+    if num!=0 {
+        println!("{}, {}",count_xy,count_yx);
+    }
+    
+
+    return puntuacion;
+
 }
+
+
+
 
 fn ia_play(matriu: [[i32; SIZE]; SIZE], x: &mut usize, y: &mut usize) {
     let mut valoracion = [[0; SIZE]; SIZE];
+    let mut valor=0;
 
     for i in 0..SIZE {
         for j in 0..SIZE {
             if matriu[i][j] != 0 {
                 valoracion[i][j] = -1;
             } else {
-                valoracion[i][j] = 5 * count_consecutive(matriu, i, j, 2);
-                if count_consecutive(matriu, i, j, 1) >= 3 {
-                    valoracion[i][j] += 1000;
-                }
-
-                if count_consecutive(matriu, i, j, 2) >= 3 {
-                    valoracion[i][j] += 10000;
-                }
+                valor += count_consecutive(matriu, i, j, 2);
+                valor += count_consecutive(matriu, i, j, 1);
+                valor += count_consecutive(matriu, i, j, 0);
+                valoracion[i][j]=valor;
+                valor=0;
             }
         }
     }
+
+    for x in 0..SIZE  {
+        for y in 0..SIZE  {
+            print!("{} ",valoracion[x][y]);
+        }
+        println!();
+    } 
 
     let mut max_num = 0;
     let mut max_coords = Vec::new();
